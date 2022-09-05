3d4b3db42675ac0b52ba0c61ec45ef26989306d3
Ricardo Heinzmann <rheinzmann@student.ethz.ch>
2022-09-05 19:33:01 +0200

    basic route adjusted to /ml-class

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import "./App.css";
-import { Button, Card, Center, Grid, Stack, Title, Text, Kbd } from "@mantine/core";
+import {
+  Button,
+  Card,
+  Center,
+  Grid,
+  Stack,
+  Title,
+  Text,
+  Kbd,
+} from "@mantine/core";
 import {
   BrowserRouter as Router,
   Routes,
@@ -38,72 +47,75 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route
-            path=""
-            element={
-              <Stack>
-                {gameTitle}
-                <Link to="/level-selection">
-                  <Stack justify="flex-start" align="center">
-                    <RobotWrapper
-                      notSticky
-                      message={<>Bist du bereit?</>}
-                    ></RobotWrapper>
-                    <Title order={2}>Klicke hier um zu beginnen</Title>
-                  </Stack>
-                </Link>
-              </Stack>
-            }
-          ></Route>
-          <Route
-            path="level-selection"
-            element={
-              <div>
-                {gameTitle}
-                <Center>
-                  <Stack align="center">
-                    <Title order={4}>Wie man spielt:</Title>
-                    <Text>
-                      Benutze die <Kbd>Knöpfe</Kbd> oben rechts um die Attribute
-                      auszuwählen, die auf den Axen angezeigt werden
-                    </Text>
-                    <Text>
-                      Du kannst auch auf die <Kbd>Tabellen</Kbd> der Klassen links klicken, um
-                      die Klassen auszuwählen die du klassifizieren möchtest.
-                      Die ausgewählten Klassen sind golden umrandet.
-                    </Text>
-                    <RobotWrapper
-                      message={<>Welches Level möchtest du spielen?</>}
-                      notSticky
-                    ></RobotWrapper>
-                    {allLevelSections.map(({ sectionHeader, levels }) => {
-                      return (
-                        <div key={sectionHeader.key + "section"}>
-                          {sectionHeader}                          
-                          <Grid justify="center">
-                            {levels.map((l) => generateButtonFromLevel(l))}
-                          </Grid>
-                        </div>
-                      );
-                    })}
-                  </Stack>
-                </Center>
-              </div>
-            }
-          ></Route>
-          <Route
-            path="level"
-            element={
-              <Stack style={{ paddingTop: "10px" }}>
-                <BackToLevelSelectionButton />
-                <Outlet></Outlet>
-              </Stack>
-            }
-          >
-            {getAllLevelRoutes()}
+          <Route path="ml-class" element={<Outlet></Outlet>}>
+            <Route
+              path=""
+              element={
+                <Stack>
+                  {gameTitle}
+                  <Link to="/level-selection">
+                    <Stack justify="flex-start" align="center">
+                      <RobotWrapper
+                        notSticky
+                        message={<>Bist du bereit?</>}
+                      ></RobotWrapper>
+                      <Title order={2}>Klicke hier um zu beginnen</Title>
+                    </Stack>
+                  </Link>
+                </Stack>
+              }
+            ></Route>
+            <Route
+              path="level-selection"
+              element={
+                <div>
+                  {gameTitle}
+                  <Center>
+                    <Stack align="center">
+                      <Title order={4}>Wie man spielt:</Title>
+                      <Text>
+                        Benutze die <Kbd>Knöpfe</Kbd> oben rechts um die
+                        Attribute auszuwählen, die auf den Axen angezeigt werden
+                      </Text>
+                      <Text>
+                        Du kannst auch auf die <Kbd>Tabellen</Kbd> der Klassen
+                        links klicken, um die Klassen auszuwählen die du
+                        klassifizieren möchtest. Die ausgewählten Klassen sind
+                        golden umrandet.
+                      </Text>
+                      <RobotWrapper
+                        message={<>Welches Level möchtest du spielen?</>}
+                        notSticky
+                      ></RobotWrapper>
+                      {allLevelSections.map(({ sectionHeader, levels }) => {
+                        return (
+                          <div key={sectionHeader.key + "section"}>
+                            {sectionHeader}
+                            <Grid justify="center">
+                              {levels.map((l) => generateButtonFromLevel(l))}
+                            </Grid>
+                          </div>
+                        );
+                      })}
+                    </Stack>
+                  </Center>
+                </div>
+              }
+            ></Route>
+            <Route
+              path="level"
+              element={
+                <Stack style={{ paddingTop: "10px" }}>
+                  <BackToLevelSelectionButton />
+                  <Outlet></Outlet>
+                </Stack>
+              }
+            >
+              {getAllLevelRoutes()}
+              <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
+            </Route>
             <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
           </Route>
-          <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
         </Routes>
       </Router>
     </div>
